8b94850cb83a1b36f835694f858b9020d287fac0
Nnamdi Nwajagu <nnamdi.nwajagu@gmail.com>
2019-02-25 15:53:35 -0500

    Haptic Controller Class

---
+++ Adafruit_BNO055/examples/sensorapi/sensorapi.cpp
@@ -0,0 +1,169 @@
+
+#include "Adafruit_Sensor.h"
+#include "Adafruit_BNO055.h"
+#include "utility/imumaths.h"
+
+/* This driver uses the Adafruit unified sensor library (Adafruit_Sensor),
+   which provides a common 'type' for sensor data and some helper functions.
+
+   To use this driver you will also need to download the Adafruit_Sensor
+   library and include it in your libraries folder.
+
+   You should also assign a unique ID to this sensor for use with
+   the Adafruit Sensor API so that you can identify this particular
+   sensor in any data logs, etc.  To assign a unique ID, simply
+   provide an appropriate value in the constructor below (12345
+   is used by default in this example).
+
+   Connections
+   ===========
+   Connect SCL to analog 5
+   Connect SDA to analog 4
+   Connect VDD to 3-5V DC
+   Connect GROUND to common ground
+
+   History
+   =======
+   2015/MAR/03  - First release (KTOWN)
+   2015/AUG/27  - Added calibration and system status helpers
+*/
+
+/* Set the delay between fresh samples */
+#define BNO055_SAMPLERATE_DELAY_MS (100)
+
+Adafruit_BNO055 bno = Adafruit_BNO055(55);
+
+/**************************************************************************/
+/*
+    Displays some basic information on this sensor from the unified
+    sensor API sensor_t type (see Adafruit_Sensor for more information)
+*/
+/**************************************************************************/
+void displaySensorDetails(void)
+{
+  sensor_t sensor;
+  bno.getSensor(&sensor);
+  printf("------------------------------------\n");
+  printf  ("Sensor:       %d\n",sensor.name);
+  printf  ("Driver Ver:   %d\n",sensor.version);
+  printf  ("Unique ID:    %d\n",sensor.sensor_id);
+  printf  ("Max Value:    %d\n",sensor.max_value);
+  printf  ("Min Value:    %d\n"),sensor.min_value);
+  printf  ("Resolution:   ",sensor.resolution);
+  printf("------------------------------------\n");
+  printf("");
+  delay(500);
+}
+
+/**************************************************************************/
+/*
+    Display some basic info about the sensor status
+*/
+/**************************************************************************/
+void displaySensorStatus(void)
+{
+  /* Get the system status values (mostly for debugging purposes) */
+  uint8_t system_status, self_test_results, system_error;
+  system_status = self_test_results = system_error = 0;
+  bno.getSystemStatus(&system_status, &self_test_results, &system_error);
+
+  /* Display the results in the Serial Monitor */
+  printf("");
+  printf("System Status: %(system_status, HEX);
+  printf("Self Test:     0x");
+  printf(self_test_results, HEX);
+  printf("System Error:  0x");
+  printf(system_error, HEX);
+  printf("");
+  delay(500);
+}
+
+/**************************************************************************/
+/*
+    Display sensor calibration status
+*/
+/**************************************************************************/
+void displayCalStatus(void)
+{
+  /* Get the four calibration values (0..3) */
+  /* Any sensor data reporting 0 should be ignored, */
+  /* 3 means 'fully calibrated" */
+  uint8_t system, gyro, accel, mag;
+  system = gyro = accel = mag = 0;
+  bno.getCalibration(&system, &gyro, &accel, &mag);
+
+  /* The data should be ignored until the system calibration is > 0 */
+  printf("\t");
+  if (!system)
+  {
+    printf("! ");
+  }
+
+  /* Display the individual values */
+  printf("Sys:%d",system);
+  printf(" G:%d",gyr);
+  printf(" A:%d",accel);
+  printf(" M:%d",mag);
+}
+
+/**************************************************************************/
+/*
+    Arduino setup function (automatically called at startup)
+*/
+/**************************************************************************/
+void setup(void)
+{
+  Serial.begin(9600);
+  printf("Orientation Sensor Test"); printf("");
+
+  /* Initialise the sensor */
+  if(!bno.begin())
+  {
+    /* There was a problem detecting the BNO055 ... check your connections */
+    printf("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
+    while(1);
+  }
+
+  delay(1000);
+
+  /* Display some basic information on this sensor */
+  displaySensorDetails();
+
+  /* Optional: Display current status */
+  displaySensorStatus();
+
+  bno.setExtCrystalUse(true);
+}
+
+/**************************************************************************/
+/*
+    Arduino loop function, called once 'setup' is complete (your own code
+    should go here)
+*/
+/**************************************************************************/
+void loop(void)
+{
+  /* Get a new sensor event */
+  sensors_event_t event;
+  bno.getEvent(&event);
+
+  /* Display the floating point data */
+  printf("X: ");
+  printf(event.orientation.x, 4);
+  printf("\tY: ");
+  printf(event.orientation.y, 4);
+  printf("\tZ: ");
+  printf(event.orientation.z, 4);
+
+  /* Optional: Display calibration status */
+  displayCalStatus();
+
+  /* Optional: Display sensor status (debug only) */
+  //displaySensorStatus();
+
+  /* New line for the next sample */
+  printf("");
+
+  /* Wait the specified delay before requesting nex data */
+  delay(BNO055_SAMPLERATE_DELAY_MS);
+}
